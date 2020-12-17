b2fe6202de62bb6d2106c4c9e9e517f6162495da
Matt Blackwell <well_have_less_o_that_matey@hotmail.com>
2020-12-17 22:44:14 +0000

    Program structure. Subclasses.
    changed program structure so that each selection window is run from a method contained in its parent and additionally created Base classes which each version of the class inherits from.

--- src/logbook_db.py
+++ src/logbook_db.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
-db_path = '../data/logbook_data.db'
+import sqlite3 as sqlite3
+
+db_path = './data/logbook_data.db'
 
 #db_name = ''
 #db_user = ''
 #db_password = ''
 #db_host = ''
 
-V = 23
+
+def search_selection(term, source):
+    ##Search result lists second parameter e.g. origin --lists--> name
+    source_dict = { "origin" : '''SELECT id, name, icao_code, iata_code
+                                  FROM airports WHERE name LIKE ?;''',
+                    "destination" : ('id, name', 'airports', str(term),)
+                   }
+    term_string = "%"+term+"%"
+    dbcon = sqlite3.connect(db_path)
+    result = dbcon.execute(source_dict[source], (term_string,)).fetchall()
+    dbcon.close()
+    
+    return result
--- src/logbook_gui.py
+++ src/logbook_gui.py
@@ -7,27 +7,10 @@ import tkcalendar as tkcal
 import logbook_db as db
 
 
-def record_window(source):#, datalist):
-    record_window = tk.Tk()
-    record_window.title(source)
-    record_frame = RecordWindow(record_window)
-    record_frame.pack()
-
-def select_window(source):
-    select_window = tk.Tk()
-    title_str = str("Select " + source + "...")
-    select_window.title(title_str)
-    select_frame = SelectWindow(select_window)
-    select_frame.pack()
-
-def search_selection(term):
-    pass
-
-
 class Application(ttk.Frame):
 
     def __init__(self, master= None, *args, **kwargs):
-        super().__init__(master = master)
+        super().__init__(*args, **kwargs)
         self.pack()
         self.config(width = 680)
         self.create_application()
@@ -39,7 +22,7 @@ class Application(ttk.Frame):
 class Taskbar(ttk.Frame):
 
     def __init__(self, master = None, *args, **kwargs):
-        super().__init__(master = master)
+        super().__init__(*args, **kwargs)
         self.create_buttons()
 
     def create_buttons(self):
@@ -47,53 +30,102 @@ class Taskbar(ttk.Frame):
         self.view_dashboard.pack()
         self.view_flightlist = ttk.Button(self, text = "Flight List View")
         self.view_flightlist.pack()
-        
         self.function_newrecord = ttk.Button(self, text = "New Record",
-                       command = lambda: record_window(source = "New Flight"))
+                                   command = lambda: self.new_flight_record())
         self.function_newrecord.pack()
+        
+    def new_flight_record(self):
+        record_window = FlightRecordWindow(self)
+        record_window.title("New Flight")
 
 
 class Dashboard(ttk.Frame):
 
     def __init__(self, master = None, *args, **kwargs):
-        super().__init__(master = master)
+        super().__init__(*args, **kwargs)
         
-
-class RecordWindow(ttk.Frame):
+class BaseRecordWindow(tk.Toplevel):
     
     def __init__(self, master = None, *args, **kwargs):
-        super().__init__(master = master)
+        super().__init__(*args, **kwargs)
+        self.create_base_widgets()
+    
+    def create_base_widgets(self):
+        self.field_frame = ttk.Frame(self)
+        self.field_frame.pack()
+      
+class FlightRecordWindow(BaseRecordWindow):
+    
+    def __init__(self, master = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.create_record_widgets()
         
     def create_record_widgets(self):
-        self.field_frame = ttk.Frame(self)
-        self.field_frame.pack()
         self.origin_label = ttk.Label(self, text = "Origin Airport")
         self.origin_label.pack()
         self.origin_entry = ttk.Entry(self, state = "readonly")
         self.origin_entry.pack()
         self.origin_select = ttk.Button(self, text = "Select...",
-                           command = lambda: select_window(source = "origin"))
+                            command = lambda: self.origin_select_window())
         self.origin_select.pack()
 #        self.date
         self.save_button = ttk.Button(self, text = "Save")
         self.save_button.pack()
         self.cancel_button = ttk.Button(self, text = "Cancel")
         self.cancel_button.pack()
+        
+    def origin_select_window(self):
+    ##need to select class of SelectWindow
+        origin_select_window = OriginSelectWindow(self, source = "origin")
+        origin_select_window.title("Select Origin...")
+        origin_select_window.geometry("500x350")
 
-
-class SelectWindow(ttk.Frame):
-
-    def __init__(self, master = None, *args, **kwargs):
-        super().__init__(master = master)
-        self.create_select_widgets()
+class BaseSelectWindow(tk.Toplevel):
     
-    def create_select_widgets(self):
-        self.select_entry = ttk.Entry(self)
+    def __init__(self, master = None, source = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.source = source
+        self.search_results = []
+        self.create_base_widgets()
+    
+    def create_base_widgets(self):
+        # input frame
+        self.input_frame = ttk.Frame(self)
+        self.input_frame.pack()
+        self.select_entry = ttk.Entry(self.input_frame)
         self.select_entry.pack()
-        self.select_button = ttk.Button(self, text = "Search", 
-                  command = lambda: search_selection(self.select_entry.get()))
+        self.select_button = ttk.Button(self.input_frame, text = "Search", 
+                                       command = lambda: self.search_update())
         self.select_button.pack()
-        self.select_list = ttk.Scrollbar(self)
+        # results
+        self.select_list = tk.Listbox(self)
         self.select_list.pack()
+        # result info
+        self.info_frame = ttk.Frame(self)
+        self.info_frame.pack()
+    
+    def search_update(self):
+        self.select_list.delete(0, tk.END)
+        self.search_results = db.search_selection(self.select_entry.get(),
+                                                  self.source)
+        for result in self.search_results:
+            self.select_list.insert(tk.END, result[1])
+        print(self.search_results)
 
+
+class OriginSelectWindow(BaseSelectWindow):
+    
+    def __init__(self, master = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.create_custom_widgets()
+    
+    def create_custom_widgets(self):
+        self.name_label_var = tk.StringVar()
+        self.name_label = ttk.Label(self.info_frame,
+                                    text = self.name_label_var).pack()
+        self.icaocode_label_var = tk.StringVar()
+        self.icaocode_label = ttk.Label(self.info_frame,
+                                        text = self.icaocode_label_var).pack()
+        self.iatacode_label_var = tk.StringVar()
+        self.iatacode_label = ttk.Label(self.info_frame,
+                                        text = self.iatacode_label_var).pack()
